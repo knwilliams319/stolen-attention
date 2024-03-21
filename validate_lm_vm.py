6651a26a1e9c5efb04b8b5c897bb614f3a54486c
knwilliams319 <knwilliams319@gmail.com>
2024-03-21 22:30:40 +0000

    Changes to create and visualize loss statistics for sliding window validation on model checkpoints

--- validate_lm_vm.py
+++ validate_lm_vm.py
@@ -9,6 +9,8 @@ from lightning.pytorch.profilers import AdvancedProfiler
 from pathlib import Path
 from sentencepiece import SentencePieceProcessor
 from lightning.pytorch.loggers import CSVLogger
+import pandas as pd
+import numpy as np
 import pickle
 # from transformers import GPT2TokenizerFast
 
@@ -70,9 +72,22 @@ class FlattenedWikitext103Dataset(data.Dataset):
 # !SECTION
 
 # SECTION: Model definitions
-global sliding_mode # if True (default false), only the predictions on the last token of the batch will influence loss
+global sliding_mode # if True (default False), only the predictions on the last token of the batch will influence loss
 
 class Wikitext103Model(CausalTransformer):
+    def __init__(self, **model_kwargs):
+        # Initialize model as per usual, but add extra state to track token statistics
+        super().__init__(**model_kwargs)
+        self.losses = {}      # KEY: token_id; VALUE: list of loss values for the prediction over that token
+        # self.norms = {}       # KEY: (layer_idx, head_idx, token_id); VALUE: list of embedding norms for that token in that head of that layer
+        # self.is_vertex = {}   # KEY: (layer_idx, head_idx, token_id); VALUE: list of vertex membership for that token in that head of that layer
+        for t in range(self.hparams.num_classes):
+            self.losses[t] = []
+            # for l in range(self.hparams.num_layers):
+            #     for h in range(self.hparams.num_heads):
+            #         self.norms[(l, h, t)] = []
+            #         self.is_vertex[(l, h, t)] = []
+
     def _calculate_loss(self, batch):
         data, labels, mask = batch
         data = data.int()
@@ -82,13 +97,24 @@ class Wikitext103Model(CausalTransformer):
             # Get predictions over all tokens in all batches
             preds = preds.view(-1, preds.size(-1))
             labels = labels.view(-1).long()
+            loss = F.cross_entropy(preds, labels)  
+            return loss
         else:
-            # Grab only the predictions on the last element of the context for all batches
-            preds = preds[:, -1]
-            labels = labels[:, -1].long()
+            # Grab predictions on the last element of the context for all batches
+            # NOTE: Using -1 will get predictions on the 513th element given the previous 512, which is what we 
+            #       generally want to measure with sliding window inference. But, we can't measure the norm of the
+            #       embedding of elements that don't get crunched by the model's attention heads.
+            preds = preds[:, -2]
+            labels = labels[:, -2].long()
 
-        loss = F.cross_entropy(preds, labels)  
-        return loss
+            # Track statistics
+            loss = F.cross_entropy(preds, labels, reduction='none')
+            labels_cpu = labels.cpu().numpy()
+            loss_cpu = loss.detach().cpu()
+            for batch_idx in range(loss_cpu.size(0)):
+                token_id = labels_cpu[batch_idx]
+                self.losses[token_id].append(loss_cpu[batch_idx].item())
+            return torch.mean(loss)  # return mean so that logging doesn't error
 
     def training_step(self, batch, batch_idx):
         loss = self._calculate_loss(batch)
@@ -162,8 +188,8 @@ class Wikitext103Model(CausalTransformer):
   
 # SECTION: Training parameters
 # TODO: make these CLI arguments instead of constants 
-CHECKPOINT_BASE = "./experiments/embed_dim_64/n_heads_32"
-EXPERIMENT = "euc"
+CHECKPOINT_BASE = "./experiments/embed_dim_64/n_heads_8"
+EXPERIMENT = "man-positional"
 CHECKPOINT_DIR = CHECKPOINT_BASE + '/' + EXPERIMENT
 VALID_PATH = "./data/wikitext-103/unigram.wiki.valid.tokens.tokenized.pt"
 TOKENIZER_PATH = "./unigram-tokenizer/tokenizer.model"
@@ -174,23 +200,23 @@ if __name__ == "__main__":
     # Set up for training. Set random seeds and initialize Trainer. 
     L.seed_everything(7, workers=True)
     trainer = L.Trainer(
-        deterministic=False,        # Doesn't matter since val_loaders aren't shuffling 
+        deterministic=False,      # Doesn't matter since val_loaders aren't shuffling 
         default_root_dir=None,
         enable_progress_bar=True,
-        accelerator="gpu",          # Uses 'mps' automatically on my Mac.
+        accelerator="gpu",          
         strategy="ddp",
-        devices=[1],                # Only one core for mps
-        precision="16-mixed",       # NOTE: Might need to be 32-true depending on the checkpoint
+        devices=[1],                  
+        precision="16-mixed",     # NOTE: Might need to be 32-true depending on the checkpoint
         benchmark=True,
-        logger=False,               # Turns off creation of 'lightning_logs' directory
-        limit_test_batches=100      # Might need to use this for higher dimensional models
+        logger=False,             # Turns off creation of 'lightning_logs' directory
+        limit_test_batches=None   # Might need to use this for higher dimensional models
     )
 
     # Initialize tokenizer
     tokenizer = SentencePieceProcessor(model_file=TOKENIZER_PATH)
 
     # Create dataloaders
-    BATCH_SIZE = 1
+    BATCH_SIZE = 448
     val_dataset = Wikitext103Dataset(VALID_PATH, tokenizer.pad_id(), len(tokenizer))
     val_loader = data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=4, persistent_workers=False)
 
@@ -198,7 +224,7 @@ if __name__ == "__main__":
     #       that the total inference time would be 1.5 hours on my Mac. The normal validation set has 264 batches, which is a large enough
     #       number to get some rough estimates for now.
     val_dataset_flat = FlattenedWikitext103Dataset(VALID_PATH, tokenizer.pad_id(), len(tokenizer))
-    val_loader_flat = data.DataLoader(val_dataset_flat, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=4, persistent_workers=False)
+    val_loader_flat = data.DataLoader(val_dataset_flat, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=3, persistent_workers=True)
 
     # Load pretrained model
     checkpoint_dir = Path(CHECKPOINT_DIR)
@@ -210,16 +236,33 @@ if __name__ == "__main__":
         raise FileNotFoundError(f'No checkpoint exists at {pretrained_file_path}!')
     
     # Validate model
-    sliding_mode = False
-    print("Testing Normal Inference on Validation Set")
-    trainer.test(model, dataloaders=val_loader, verbose=True)
-    with open(f'q-hull-props-{EXPERIMENT}.pkl', 'wb') as q_hull_file:
-        pickle.dump(model.q_hull_props,q_hull_file)
-    with open(f'k-hull-props-{EXPERIMENT}.pkl', 'wb') as k_hull_file:
-        pickle.dump(model.k_hull_props,k_hull_file)
+    # print("Testing Normal Inference on Validation Set")
+    # sliding_mode = False
+    # trainer.test(model, dataloaders=val_loader, verbose=True)
+    # with open(f'q-hull-props-{EXPERIMENT}.pkl', 'wb') as q_hull_file:
+    #     pickle.dump(model.q_hull_props,q_hull_file)
+    # with open(f'k-hull-props-{EXPERIMENT}.pkl', 'wb') as k_hull_file:
+    #     pickle.dump(model.k_hull_props,k_hull_file)
 
-    # print("Testing Sliding Window Inference on Validation Set")
-    # sliding_mode = True
-    # trainer.test(model, dataloaders=val_loader_flat, verbose=True)
+    print("Testing Sliding Window Inference on Validation Set")
+    sliding_mode = True
+    trainer.test(model, dataloaders=val_loader_flat, verbose=True)
+    avg_losses = [pd.NA]*16000
+    variances = [pd.NA]*16000
+    counts = [pd.NA]*16000
+    for token_id, losses_array in model.losses.items():
+        if losses_array:
+            avg_losses[token_id] = np.mean(losses_array)
+            variances[token_id] = np.var(losses_array)
+            counts[token_id] = len(losses_array)
+    statistics = pd.DataFrame({
+        'token_id': np.arange(16000),
+        'avg_loss': avg_losses,
+        'loss_variance': variances,
+        'val_freq': counts
+    })
+    statistics_path = checkpoint_dir / f'val-loss-stats.csv'
+    statistics.to_csv(statistics_path, index=False)
+    print("Done")
 
 #!SECTION
